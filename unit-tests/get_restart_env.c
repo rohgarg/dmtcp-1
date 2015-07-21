413d88e424cb0e347886d303dd9a498e87c098d4
Rohan Garg <rohgarg@ccs.neu.edu>
2015-07-21 10:58:18 -0400

    unit-tests: add tests for dmtcp_get_restart_env()

---
+++ unit-tests/get_restart_env.c
@@ -0,0 +1,101 @@
+#include <assert.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+extern int dmtcp_protected_environ_fd();
+
+static int
+readLine(int fd, char *buf, int count)
+{
+  int i = 0;
+  char c;
+  if (fd < 0 || buf == NULL) return -1;
+#define NEWLINE '\n' // Linux, OSX
+  while (i < count) {
+    ssize_t rc = read(fd, &c, 1);
+    if (rc == 0) {
+      break;
+    } else if (rc < 0) {
+      buf[i] = '\0';
+      return -1;
+    } else {
+      buf[i++] = c;
+      if (c == NEWLINE) break;
+    }
+  }
+  buf[i] = '\0';
+  if (i >= count)
+    return -2;
+  else
+    return i;
+}
+
+#define SUCCESS 0
+#define NOTFOUND -1
+#define TOOLONG -2
+#define DMTCP_BUF_TOO_SMALL -3
+#define INTERNAL_ERROR -4
+#define NULL_PTR -5
+#define MAXSIZE 3000
+
+int
+dmtcp_get_restart_env(const char *name,   // IN
+                      char *value,        // OUT
+                      int maxvaluelen)
+{
+  int env_fd = dup(dmtcp_protected_environ_fd());
+  if(env_fd == -1) { return INTERNAL_ERROR; }
+  lseek(env_fd, 0, SEEK_SET);
+  int namelen = strlen(name);
+  *value = '\0'; // Default is null string
+
+  int rc = NOTFOUND; // Default is -1: name not found
+
+  char env_buf[MAXSIZE] = {0}; // All "name=val" strings must be shorter than this.
+
+  if (name == NULL || value == NULL) {
+    close(env_fd);
+    return NULL_PTR;
+  }
+
+  char *pos = NULL;
+
+  while (rc == NOTFOUND) {
+   memset(env_buf, 0, MAXSIZE);
+   // read a flattened name-value pairs list
+   int count = readLine(env_fd, env_buf, MAXSIZE);
+   if (count == 0) {
+     break;
+   } else if (count == -1) {
+     rc = INTERNAL_ERROR;
+   } else if (count == -2) {
+     rc = DMTCP_BUF_TOO_SMALL;
+   } else {
+     char *start_ptr = env_buf;
+     // iterate over the flattened list of name-value pairs
+     while (start_ptr - env_buf < sizeof(env_buf)) {
+       pos = NULL;
+       if (strncmp(start_ptr, name, namelen) == 0) {
+         if ((pos = strchr(start_ptr, '='))) {
+           strncpy(value, pos + 1, maxvaluelen);
+           if (strlen(pos+1) > maxvaluelen) {
+             rc = TOOLONG; // value does not fit in the user-provided value buffer
+             break;
+           }
+         }
+         rc = SUCCESS;
+         break;
+       }
+       // skip over a name-value pair
+       start_ptr += strlen(start_ptr) + 1;
+     }
+   }
+  }
+
+  close(env_fd);
+  return rc;
+}
---
+++ unit-tests/get_restart_env_test.c
@@ -0,0 +1,118 @@
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <assert.h>
+
+#include "minunit.h"
+
+#define EFD "/proc/self/environ"
+
+static int gfd = -1;
+
+extern int dmtcp_get_restart_env(const char *name,   // IN
+                                 char *value,        // OUT
+                                 int maxvaluelen);
+
+int
+dmtcp_protected_environ_fd()
+{
+  return gfd;
+}
+
+void test_setup()
+{
+  gfd = open(EFD, O_RDONLY);
+  assert(gfd > 0);
+}
+
+void test_teardown()
+{
+  assert(close(gfd) == 0);
+}
+
+MU_TEST(test_empty_file)
+{
+#undef SIZE
+#define SIZE 10
+  const char *name = "name";
+  char value[SIZE] = {0};
+  int temp = gfd;
+  gfd = -1;
+  int rc = dmtcp_get_restart_env(name, value, SIZE);
+  mu_assert(rc == -4,
+            "invalid fd should return INTERNAL_ERROR(-4)");
+  gfd = temp;
+}
+
+MU_TEST(test_nonexistent_env_var)
+{
+#undef SIZE
+#define SIZE 10
+  const char *name = "name";
+  char value[SIZE] = {0};
+  int rc = dmtcp_get_restart_env(name, value, SIZE);
+  mu_assert(rc == -1,
+            "non-existent env var should return -1");
+}
+
+MU_TEST(test_existent_env_var)
+{
+#undef SIZE
+#define SIZE 10
+  const char *name = "USER";
+  char value[SIZE] = {0};
+  int rc = dmtcp_get_restart_env(name, value, SIZE);
+  mu_assert(rc == 0,
+            "existent env var should return 0");
+  mu_assert(strncmp(value,getenv(name),SIZE) == 0,
+            "value for existent env var should match"
+            " that returned by getenv()");
+}
+
+MU_TEST(test_short_buf)
+{
+#undef SIZE
+#define SIZE 10
+  const char *name = "PATH";
+  char value[SIZE] = {0};
+  int rc = dmtcp_get_restart_env(name, value, SIZE);
+  mu_assert(rc == -2,
+            "short buf should return -2");
+}
+
+MU_TEST(test_short_dmtcp_buf)
+{
+#undef SIZE
+#define SIZE 10
+  const char *name = "PATH";
+  char value[SIZE] = {0};
+  int temp = gfd;
+  system("cat /proc/self/environ > /tmp/unit-test-101");
+  system("cat /proc/self/environ >> /tmp/unit-test-101");
+  gfd = open("/tmp/unit-test-101", O_RDONLY);
+  assert(gfd > 0);
+  int rc = dmtcp_get_restart_env(name, value, SIZE);
+  mu_assert(rc == -3,
+            "dmtcp short buf should return -3");
+  close(gfd);
+  gfd = temp;
+}
+
+MU_TEST_SUITE(test_suite)
+{
+  MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+  MU_RUN_TEST(test_empty_file);
+  MU_RUN_TEST(test_nonexistent_env_var);
+  MU_RUN_TEST(test_existent_env_var);
+  MU_RUN_TEST(test_short_buf);
+  MU_RUN_TEST(test_short_dmtcp_buf);
+}
+
+int main(int argc, char *argv[])
+{
+  MU_RUN_SUITE(test_suite);
+  MU_REPORT();
+  return 0;
+}
