e91915cecf4da3e61c92a9d9edfe3ece2b84e67c
Gregory Price <price.gr@husky.neu.edu>
2018-03-23 09:05:53 -0600

    irecv/isend/test/wait test cases

---
+++ contrib/mpi-proxy/test/async/isend_irecv.c
@@ -0,0 +1,36 @@
+// this should test that Isend and Irecv can be used together in the simplest
+// way possible (effectively re-implementing MPI_Send and MPI_Recv)
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+  // Initialize the MPI environment
+  MPI_Init(NULL, NULL);
+  // Find out rank, size
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  MPI_Request request = 0;
+
+  // We are assuming at least 2 processes for this task
+  if (world_size < 2) {
+    fprintf(stderr, "World size must be greater than 1 for %s\n", argv[0]);
+    MPI_Abort(MPI_COMM_WORLD, 1);
+  }
+
+  int number;
+  if (world_rank == 0) {
+    // If we are rank 0, set the number to -1 and send it to process 1
+    number = -1;
+    MPI_Isend(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, &request);
+    MPI_Wait(&request, MPI_STATUS_IGNORE);
+  } else if (world_rank == 1) {
+    MPI_Irecv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
+    MPI_Wait(&request, MPI_STATUS_IGNORE);
+    printf("Process 1 received number %d from process 0\n", number);
+  }
+  MPI_Finalize();
+}
